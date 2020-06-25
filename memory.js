da5e886627bc1c94372e61899eb70305e0bf3310
AlicjaLysien <61288677+AlicjaLysien@users.noreply.github.com>
2020-06-25 08:58:19 +0200

    mix cards

--- memory.js
+++ memory.js
@@ -4,14 +4,11 @@ var cards = ["01.png", "05.png", "06.png", "02.png", "04.png", "03.png",
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
   
-    // While there remain elements to shuffle...
     while (0 !== currentIndex) {
   
-      // Pick a remaining element...
       randomIndex = Math.floor(Math.random() * currentIndex);
       currentIndex -= 1;
-  
-      // And swap it with the current element.
+
       temporaryValue = array[currentIndex];
       array[currentIndex] = array[randomIndex];
       array[randomIndex] = temporaryValue;
